b05c5fc24ada471b3845e302b1373997c8e9aa96
chriskottom <chris@chriskottom.com>
2017-07-05 11:22:04 +0200

    Populate basic data values for product links

--- lib/onebox/engine/thewinesociety_onebox.rb
+++ lib/onebox/engine/thewinesociety_onebox.rb
@@ -1,6 +1,8 @@
 module Onebox
   module Engine
     class TheWineSocietyOnebox
+      MAX_DESCRIPTION_CHARS = 300
+
       include Engine
       include LayoutSupport
       include HTML
@@ -13,13 +15,46 @@ module Onebox
 
       private
 
+      def image
+        product_image = raw.at_css('.pnl-product-image img')
+        if product_image
+          product_image[:src]
+        end
+      end
+
+      def title
+        title_node = raw.at_css('h1.productName')
+        if title_node
+          title_node.inner_html
+        end
+      end
+
+      def description(text = nil)
+        if !text
+          all_content_node = raw.at_css('.pnl-product-detail-description .allcontent')
+          if all_content_node
+            text = all_content_node.inner_html
+          else
+            description_node = raw.at_css('.pnl-product-detail-description')
+            if description_node
+              text = description_node.text
+            end
+          end
+        end
+
+        Onebox::Helpers.truncate(text, MAX_DESCRIPTION_CHARS)
+      end
+
       def data
+        og   = ::Onebox::Helpers.extract_opengraph(raw)
+        prod = ::Onebox::Helpers.extract_product_info(raw)
+
         {
-          image: 'https://placekitten.com/221/332',
-          link: 'http://example.com/',
-          title: 'Test Title',
-          description: 'Test Description',
-          price: '0.01'
+          image: og[:image] || image,
+          link: link,
+          title: og[:title] || title,
+          description: description(og[:description]),
+          price: prod[:price_amount]
         }
       end
     end
--- spec/lib/onebox/engine/thewinesociety_onebox_spec.rb
+++ spec/lib/onebox/engine/thewinesociety_onebox_spec.rb
@@ -1,4 +1,42 @@
 require 'spec_helper'
 
 describe Onebox::Engine::TheWineSocietyOnebox do
+  let(:link) { 'https://www.thewinesociety.com/shop/ProductDetail.aspx?pd=CE8721' }
+  let(:html) { described_class.new(link).to_html }
+  let(:parsed_html) { Nokogiri::HTML(html) }
+
+  before do
+    fake(link, response('thewinesociety'))
+  end
+
+  it 'uses the image from the page' do
+    image_node = parsed_html.at_css('img.thumbnail')
+    expect(image_node[:src]).to match(%r{resources/product_images/CE8721\.jpg})
+  end
+
+  it 'uses the link from the page' do
+    header_node = parsed_html.at_css('header.source a')
+    expect(header_node[:href]).to eq(link)
+    expect(header_node.text).to eq('thewinesociety.com')
+
+    title_node = parsed_html.at_css('h3 a')
+    expect(title_node[:href]).to eq(link)
+    expect(title_node.text).to eq('Concha y Toro Corte Ignacio Casablanca Riesling 2015')
+  end
+
+  it 'uses the unaltered description from the page' do
+    description_node = parsed_html.at_css('p.description')
+    description = description_node.text
+
+    expected = 'A lovely floral Chilean riesling with some white-pepper '\
+               'aromas. The palate is just off-dry, its refreshing acidity '\
+               'balanced by a light honeyed character.'
+    expect(description).to eq(expected)
+    expect(description).not_to match(/\.\.\.$/)
+  end
+
+  it 'uses the price from the page' do
+    price_node = parsed_html.at_css('p.priceline strong .price')
+    expect(price_node.text).to eq('8.50')
+  end
 end
